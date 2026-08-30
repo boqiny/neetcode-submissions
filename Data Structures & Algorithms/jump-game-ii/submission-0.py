2c3a0b022bf3cac80f8042c6fa9bc218f2e0abca
Boqin Yuan <73152032+boqiny@users.noreply.github.com>
2026-08-29 17:02:27 -0700

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # dp[i] = min # of jumps to reach index i
+        n = len(nums)
+        dp = [float('inf')] * n
+        dp[0] = 0
+        
+        for i in range(n):
+            for j in range(1, nums[i] + 1):
+                if i + j < n:
+                    dp[i+j] = min(dp[i+j], dp[i] + 1)
+
+        return dp[-1]
+
+
+        
