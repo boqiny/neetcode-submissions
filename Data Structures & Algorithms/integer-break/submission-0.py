93a3318ae45643433ec5b9003e36da1b0a1ff776
Boqin Yuan <73152032+boqiny@users.noreply.github.com>
2026-09-06 17:00:31 -0700

    Add: integer-break - submission-0

---
+++ Data Structures & Algorithms/integer-break/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        # dp[i] := max product can reached that sum up to i
+        # dp[i] = j: 1,...,i-1 of max(dp[i], j * (i-j) , j * dp[i-j])
+        dp = [0] * (n+1)
+        dp[1] = 1
+        for i in range(2, n+1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], j * (i-j), j * dp[i-j])
+        print(dp)
+        return dp[-1]
